a280735de331558a2d242af7b851b90fa6088187
Marcus Enblom <marcusenblom@gmail.com>
2020-01-21 13:57:01 +0100

    asdd

--- hello.js
+++
@@ -1,9 +0,0 @@
-console.log('Word on the street is that blomman got a fat cock');
-
-let balls = document.createElement("div");
-
-balls.innerHTML = "<b>Big n hairy</b>"
-
-document.getElementById("fatCock").appendChild(balls)
-
-// What

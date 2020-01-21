67eceaf6ca970fd469be62accadf82bcee908a1a
Chabbe <henriksson.charlie@gmail.com>
2020-01-21 13:23:14 +0100

    The truth

--- hello.js
+++ hello.js
@@ -1,1 +1,7 @@
-console.log('Word on the street is that blomman got a fat cock')
+console.log('Word on the street is that blomman got a fat cock');
+
+let balls = document.createElement("div");
+
+balls.innerHTML = "<b>Big n hairy</b>"
+
+document.getElementById("fatCock").appendChild(balls)
